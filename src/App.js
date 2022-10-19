19f8a28f5c7fd1dc85be75e8604b54850bd1a0d6
Roberto Jimenez <robertojimenezg@gmail.com>
2022-10-19 01:26:59 -0500

    Version Final

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import CerrarSesion from './components/CerrarSesion';
 import LoginButton from './components/LoginButton';
 import Profile from './components/Profile';
+import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
+
+  const {isAuthenticated} = useAuth0();
 
   return (
     <div className="App">
       <h1>Application</h1>
-      <LoginButton />
-      <CerrarSesion />
+      {
+        isAuthenticated 
+          ?<CerrarSesion />
+          :<LoginButton />
+      }
+      
+      
       <Profile />
     </div>
   );
